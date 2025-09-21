e278cf68f970175dfc898f6d3b570a9abc86055c
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-09-21 18:49:50 +0600

    Update App.jsx

--- blog-website/src/App.jsx
+++ blog-website/src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import Developers from "./pages/Developers";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:slug" element={<Blog />} />
+        <Route path="/developers" element={<Developers />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
-
